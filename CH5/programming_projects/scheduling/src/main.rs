214f36e9e7adfc479f50740d393f43eb582de650
i3abghany <mahmoudabdalghany@outlook.com>
2021-01-27 00:11:40 +0200

    CH5: SJF: Added SJF scheduler.

--- CH5/programming_projects/scheduling/src/main.rs
+++ CH5/programming_projects/scheduling/src/main.rs
@@ -1,23 +1,25 @@
 #![allow(dead_code)]
 
-mod fcfs;
+
 mod proc;
 mod reader;
 
-use fcfs::*;
+mod sjf;
+use sjf::*;
+
 use reader::*;
 
 fn main() {
-    let mut fcfs_sched = FCFSScheduler::new();
+    let mut sjf_sched = SJFScheduler::new();
     let mut rd = ProcReader::new(String::from("./traces/fcfs.txt"));
 
     loop {
         let prc = rd.read_proc();
         match prc {
-            Some(p) => fcfs_sched.add(p),
+            Some(p) => sjf_sched.add(p),
             None => break,
         }
     }
 
-   fcfs_sched.operate(); 
+   sjf_sched.operate(); 
 }
---
+++ CH5/programming_projects/scheduling/src/sjf.rs
@@ -0,0 +1,54 @@
+#![allow(dead_code)]
+
+use crate::proc::*;
+pub struct SJFScheduler {
+    procs: Vec<Proc>,
+}
+
+impl SJFScheduler {
+    pub fn new() -> SJFScheduler {
+        SJFScheduler {
+            procs: Vec::new()
+        }
+    }
+
+    pub fn add(&mut self, t: Proc) {
+        self.procs.push(t);
+    }
+
+    fn get(&self, i: usize) -> &Proc {
+        &self.procs[i]
+    }
+
+    fn get_next(&self) -> usize {
+        let mut p = self.get(0);
+        let mut idx: usize = 0;
+        let sz = self.procs.len();
+
+        for i in 1..sz {
+            let curr_proc = self.get(i);
+            if p.get_burst() > curr_proc.get_burst() {
+                p = curr_proc;
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
+    fn schedule(&mut self) {
+        let next_idx: usize = self.get_next();
+        let p = self.get(next_idx);
+        println!("Scheduled {} for {}", p.get_name(), p.get_burst());
+        self.remove(next_idx);
+    }
+
+    fn remove(&mut self, i: usize) {
+        self.procs.remove(i);
+    }
+
+    pub fn operate(& mut self) {
+        while self.procs.len() > 0 {
+            self.schedule();
+        }
+    }
+}
